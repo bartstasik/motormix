9690fb1afd6a3ec3cd7a91231e788789743ed154
Bartosz Stasik <bart.stasik@megaslice.uk>
2023-06-18 22:16:28 +0100

    midi: ctrl Kronos timbre vol. with physical fader

--- pico/midi_stuff/code.py
+++ pico/midi_stuff/code.py
@@ -7,6 +7,8 @@ import usb_midi
 from adafruit_midi.note_on import NoteOn
 from adafruit_midi.note_off import NoteOff
 
+from analogio import AnalogIn
+
 from BartMIDI import BartMIDI, KronosMessage
 
 print(usb_midi.ports)
@@ -31,65 +33,33 @@ note_mapping = [
     ["G2", "G1"]
 ]
 
-# while True:
-#     ix = 0
-#
-#     print("note %d started" % ix)
-#     led.value = True
-#     midi.send([NoteOn(a, 60) for a in note_mapping[ix]])
-#     time.sleep(1)
-#
-#     print("note %d stopped" % ix)
-#     led.value = False
-#     midi.send([NoteOff(a, 0) for a in note_mapping[ix]])
-#     time.sleep(1)
 
-# while True:
-#     # print("sending")
-#     # msg = midi.send(msg=b'\xf0B0hC\x04\x01\x00\x02\x00\x00\x00E\xf7')
-#     # time.sleep(1)
-#     msg = midi.receive()
-#     if msg is None:
-#         continue
-#     # print(msg, buffer, channel)
-#
-#     # with open("/recorded_data.csv", "a+") as f:
-#     #     f.write(f"{buffer}\n")
+class Fader:
+    def __init__(self, fader_pin, precision=7):
+        self._potentiometer = AnalogIn(fader_pin)
+        self._bit_precision = precision
 
-# midi.send(msg)
-# print(msg)
-# time.sleep(1)
-# print(msg, msg.__str__, msg.status)
+    @property
+    def value(self):
+        return (self._potentiometer.value >> 16 - self._bit_precision) * 2
 
-# with open("/recorded_data.csv", "r") as f:
-#     lines = f.readlines()
-#
-#     for i in lines:
-#         j = i.strip()
-#         while j[-1] == ",":
-#             j = j[:-1]
-#         buffer = eval(j)
-#         print(buffer)
-#         time.sleep(1)
+
+clock_speed = 10
+
+bit_precision = 6
+
+fader1 = Fader(board.A1, bit_precision)
+
+time.sleep(3)
+
+last_value = 0
 
 while True:
-    for i in range(16):
-        for j in range(128):
-            a = KronosMessage(fader=i, position=j)
-            packet = a.to_bytearray()
-            midi.send(packet)
-            time.sleep(.001)
-
-print("Channel change")
-a = KronosMessage(b'\xf0B0hC\x04\x08\x00\x0e\x00\x00\x00\x00\xf7')
-print(a)
-print("Fader change")
-a = KronosMessage(b'\xf0B0hC\x04\x08\x00\x02\x00\x00\x00E\xf7')
-print(a)
-print("Page change")
-a = KronosMessage(b'\xf0B0hC\x1b\x00\x00\x03\x00\x00\x00\x01\xf7')
-print(a)
-
-print("Generated fader change")
-a = KronosMessage(fader=8, position=69)
-print(a, a.to_bytearray(), type(a.to_bytearray()))
+    a = fader1.value
+    if a + 2 < last_value or a - 2 > last_value:
+        last_value = a
+        print(a)
+        a = KronosMessage(fader=0, position=a)
+        packet = a.to_bytearray()
+        midi.send(packet)
+    time.sleep(0.001)
